ab4d74820de465740da6722da587d02deb762a2f
alffy007 <jamesbond006aj@gmail.com>
2022-07-24 22:22:44 +0530

    new

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Spline from '@splinetool/react-spline';
 export default function App() {
   return (
-    <Spline scene="https://prod.spline.design/OAQi2QLDTWYNsPVs/scene.splinecode" />
+    <Spline scene="https://prod.spline.design/OAQi2QLDTWYNsPVs/scene.splinecode"/>
   );
 }
 
