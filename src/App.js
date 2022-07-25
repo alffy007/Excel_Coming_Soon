27c5560a56181043d9bdb3a017c70bed9d4b1a07
alffy007 <jamesbond006aj@gmail.com>
2022-07-25 12:12:48 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,23 @@ import './App.css';
 import Spline from '@splinetool/react-spline';
 export default function App() {
   return (
-    <Spline scene="https://prod.spline.design/OAQi2QLDTWYNsPVs/scene.splinecode"/>
+    <div className='app'>
+   
+   <Spline className='logo' scene="https://prod.spline.design/lRdmK9IXz9hUY3eb/scene.splinecode" />
+   
+   <Spline className='bottom' scene="https://prod.spline.design/kQ59oNTxtnb-2s3M/scene.splinecode" />
+   
+   <Spline className='links' scene="https://prod.spline.design/JgxGMM0lr7HQbo-L/scene.splinecode" />
+   <Spline className='robot' scene="https://prod.spline.design/xosAJD8KPWdawRKp/scene.splinecode" />
+   <Spline className='guitar'  scene="https://prod.spline.design/OQtCAXTRnP27AUxf/scene.splinecode" />
+   <Spline className='mobile' scene="https://prod.spline.design/dBURL7LFSCbI0zOd/scene.splinecode" />
+   <Spline className='astronaut' scene="https://prod.spline.design/9TgkVMTRVIVcN0ae/scene.splinecode" />
+   <Spline className='camera' scene="https://prod.spline.design/RlDJ-dkIP7xL5y-S/scene.splinecode" />
+      <Spline className='controller' scene="https://prod.spline.design/EA2Uw9YxGbcTtafl/scene.splinecode" />
+
+<h1 className='helo'>EXCEL 2022</h1>
+<h2 className='soon'>We're    coming    soon</h2>
+    </div>
   );
 }
 
